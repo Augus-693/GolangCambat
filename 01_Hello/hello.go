304ae938011a35feb2528a6e66ce6c30e73236c4
Augus <auguslee529@gmail.com>
2023-07-19 16:32:01 +0800

    2023/7/29

---
+++ 01_Hello/hello.go
@@ -0,0 +1,22 @@
+package main
+
+//每个go程序都必须有一个 package main
+//每个go程序，都是 .go 结尾的
+//go 程序中没有 .h，没有 .o，只有 .go
+//一个package，包名，相当于命名空间
+//std:cout
+
+import "fmt"
+
+/**
+ * @Project GolangCambat
+ * @File    hello.go.go
+ * @Author  Augus Lee
+ * @Date    2023/7/8 7:58
+ * @GoVersion go1.20.5
+ * @Version 1.0
+ */
+
+func main() {
+	fmt.Println("Hello, World!")
+}
